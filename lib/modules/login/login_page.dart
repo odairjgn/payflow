33a90cab8bd3e07af174ae38b571b8d330f7a0f2
Odair <odairjgn@outlook.com>
2021-06-23 23:37:15 -0300

    Ajustes

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -54,7 +54,9 @@ class _LoginPageState extends State<LoginPage> {
                     padding:
                         const EdgeInsets.only(left: 40, right: 40, top: 40),
                     child: SocialLoginButton(
-                      onTap: () { print("Clicou")},
+                      onTap: () {
+                        print("Clicou");
+                      },
                     ),
                   )
                 ],
