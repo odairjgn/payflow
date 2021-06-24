d3c6b13bd8acb245599b06d0eac01e275b7ef946
Odair <odairjgn@outlook.com>
2021-06-23 23:36:22 -0300

    first commit

---
+++ winuwp/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\tools\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\nlw\\payflow" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\tools\\flutter"
+  "PROJECT_DIR=E:\\nlw\\payflow"
+  "FLUTTER_ROOT=C:\\tools\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\nlw\\payflow\\winuwp\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\nlw\\payflow"
+  "FLUTTER_TARGET=E:\\nlw\\payflow\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\nlw\\payflow\\.dart_tool\\package_config.json"
+)
